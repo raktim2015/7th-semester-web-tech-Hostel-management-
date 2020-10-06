a573db2b95e808e291ed9d36a596261496e8875a
Ritesh Singh <ritesh286singh@gmail.com>
2020-10-06 20:25:13 +0530

    Shifted Mongo Database online

--- hostel-management-server/common/config/env.config.js
+++ hostel-management-server/common/config/env.config.js
@@ -1,6 +1,7 @@
 module.exports = {
 
     "port": 4000,
+    "ATLAS_URI": "mongodb+srv://ritesh:ritesh@cluster0.jhdtg.gcp.mongodb.net/hostel?retryWrites=true&w=majority",
     "appEndPoint": "http://localhost:4000",
     "apiEndPoint": "http://localhost:4000",
     "jwt_secret": "Secret!Key6599",
--- hostel-management-server/common/services/mongoose.service.js
+++ hostel-management-server/common/services/mongoose.service.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const config = require("../config/env.config.js")
+
 let count = 0
 
 const options = {
@@ -11,7 +13,7 @@ const options = {
 
 const connectWithRetry = () => {
     console.log("MongoDB connecting..")
-    mongoose.connect("mongodb://localhost:27017/hostel",options).then(()=> {
+    mongoose.connect(config.ATLAS_URI,options).then(()=> {
         console.log("Connected")
     }).catch(err=> {
         console.log('Connection unsuccessful, retry after 5 seconds. ',++count)
